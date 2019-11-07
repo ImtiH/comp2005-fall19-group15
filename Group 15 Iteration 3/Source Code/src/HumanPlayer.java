81551c9c4d7d031d3b332fba54db44e20ce60e50
jp6337 <jp6337@mun.ca>
2019-11-07 09:54:25 -0330

    iter 3

---
+++ Group 15 Iteration 3/Source Code/src/HumanPlayer.java
@@ -0,0 +1,47 @@
+/**
+ * The HumanPlayer class holds the attributes of human players and the funcations that the human player will use to interact with the game.
+ */
+import java.util.LinkedList;
+
+public class HumanPlayer implements Player {
+	private LinkedList<Piece> pieces;
+	private boolean firstTurn;
+	private boolean playing;
+	private boolean turn;
+	private int points;
+	
+	public HumanPlayer(int color) {
+		Shapes shapes = new Shapes();
+		pieces = new LinkedList<Piece>();
+	    
+	    for (int i = 0; i < 21; i++)
+	    {
+	       pieces.add(new Piece(shapes.getShape(i), color));
+	    }
+	    this.firstTurn = true;
+	    this.playing = true;
+	    this.turn = true;
+	}
+	
+	public void move() {
+		//TODO make move function.
+	}
+	
+	public int getScore() {
+		//TODO make Score function.
+		this.points = 0;
+		return points;
+	}
+	
+	public boolean getFirstTurn() {
+		return this.firstTurn;
+	}
+	
+	public void setFirstTurn(boolean firstTurn) {
+		this.firstTurn = firstTurn;
+	}
+	
+	public LinkedList<Piece> getPieces() {
+		return pieces;
+	}
+}
